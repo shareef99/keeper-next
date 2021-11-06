94a84fe51efdcf9429069009300ac0c7d73d2aa9
shareef <nadeemshareef934@gmail.com>
2021-11-06 10:28:03 +0530

    🪓 remove unused files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,24 @@
 import type { NextPage } from "next";
+import Head from "next/head";
+import { Fragment } from "react";
 
 const Home: NextPage = () => {
     return (
-        <div>
-            <h1 className="text-red-300">Hello World!</h1>
+        <Fragment>
+            <Head>
+                <title>Keeper | Shareef</title>
+            </Head>
             <div>
-                <ul className="flex">
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                </ul>
+                <h1 className="text-red-300">Hello World!</h1>
+                <div>
+                    <ul className="flex">
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                    </ul>
+                </div>
             </div>
-        </div>
+        </Fragment>
     );
 };
 
