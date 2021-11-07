d193b7e6732fd2b39acc517d869746dd7cab99b0
shareef <nadeemshareef934@gmail.com>
2021-11-07 13:27:49 +0530

    🛠 config tailwind and extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
     purge: [
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
     ],
     darkMode: false, // or 'media' or 'class'
+    mode: "jit",
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                blue: "var(--blue)",
+                darkBlue: "var(--dark-blue)",
+                lightBlue: "var(--light-blue)",
+                cream: "var(--cream)",
+                brown: "var(--brown)",
+                lightBrown: "var(--light-brown)",
+            },
+        },
+        screens: {
+            xs: "475px",
+            ...defaultTheme.screens,
+        },
+        container: {
+            padding: "2rem",
+            center: true,
+        },
     },
     variants: {
         extend: {},
