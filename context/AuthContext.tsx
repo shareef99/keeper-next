49c3671df2709de988dd8abfbb2811552ee33176
shareef <nadeemshareef934@gmail.com>
2021-11-26 19:21:07 +0530

    ✨ updating user data to firebase

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -6,12 +6,13 @@ import {
     ReactNode,
     useEffect,
 } from "react";
+import { auth, db } from "../lib/firebase";
 import {
     GoogleAuthProvider,
     signInWithPopup,
     onAuthStateChanged,
 } from "firebase/auth";
-import { auth } from "../lib/firebase";
+import { doc, setDoc } from "firebase/firestore";
 
 interface authContextType {
     user: User | undefined;
@@ -25,6 +26,9 @@ interface User {
     emailVerified: boolean;
     photoURL: string;
     uid: string;
+    isAnonymous: boolean;
+    creationTime: string;
+    lastSignInTime: string;
 }
 
 const authContextDefaultValues: authContextType = {
@@ -34,6 +38,9 @@ const authContextDefaultValues: authContextType = {
         emailVerified: false,
         photoURL: "",
         uid: "",
+        isAnonymous: false,
+        creationTime: "",
+        lastSignInTime: "",
     },
     login: () => Promise.resolve(),
     logout: () => {},
@@ -65,13 +72,28 @@ export function AuthProvider({ children }: Props) {
                     GoogleAuthProvider.credentialFromResult(result);
                 const token = credential?.accessToken;
                 const user = result.user;
-                setUser({
+
+                const userDetails: User = {
                     displayName: user.displayName!,
                     email: user.email!,
                     emailVerified: user.emailVerified,
                     photoURL: user.photoURL!,
                     uid: user.uid,
-                });
+                    isAnonymous: user.isAnonymous,
+                    creationTime: user.metadata.creationTime!,
+                    lastSignInTime: user.metadata.lastSignInTime!,
+                };
+
+                setUser(userDetails);
+
+                try {
+                    console.log("Uploading user data to firebase");
+                    await setDoc(doc(db, "users", user.email!), userDetails);
+                    console.log("User details saved to firestore");
+                } catch (error) {
+                    console.log(error);
+                }
+
                 await router.push(`/user/${user.displayName}`);
                 console.log({ credential, token, user });
             }
@@ -104,6 +126,9 @@ export function AuthProvider({ children }: Props) {
                     emailVerified: user.emailVerified,
                     photoURL: user.photoURL!,
                     uid: user.uid,
+                    isAnonymous: user.isAnonymous,
+                    creationTime: user.metadata.creationTime!,
+                    lastSignInTime: user.metadata.lastSignInTime!,
                 });
             } else {
                 setUser(undefined);
